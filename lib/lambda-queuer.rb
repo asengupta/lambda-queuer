88e69a4c332712081a7b7c3c1b8474d1c9c22e71
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-09-19 18:39:34 +0530

    Supports answer queue alternative to EventMachine stop, actively creates queue if it is not present

--- lib/lambda-queuer.rb
+++ lib/lambda-queuer.rb
@@ -4,11 +4,12 @@ require 'sourcify'
 
 class LambdaQueuer
 	attr_accessor :queue
-	def initialize(exchange, routing_key, host='127.0.0.1', port=5672)
-		@host = host
-		@port = port
-		@exchange = exchange
-		@routing_key = routing_key
+	def initialize(options = {})
+		@host = options[:host] || '127.0.0.1'
+		@port = options[:port] || 5672
+		@exchange = options[:exchange] || 'default_exchange'
+		@request_routing_key = options[:request_routing_key] || 'default_routing_key'
+		@response_routing_key = options[:response_routing_key]
 	end
 
 	def post(&block)
@@ -17,12 +18,21 @@ class LambdaQueuer
 				connection = AMQP.connect(:host => @host, :port => @port)
 			  	channel = AMQP::Channel.new(connection)
 			  	exchange = channel.direct(@exchange, :auto_delete => true)
-			  	@queue = channel.queue(@routing_key, :auto_delete => false, :passive => true)
-			  	@queue.bind(exchange, :routing_key => @routing_key)
+			  	@queue = channel.queue(@request_routing_key, :auto_delete => true)
+			  	@queue.bind(exchange, :routing_key => @request_routing_key)
 				v = block.to_source
-			  	exchange.publish(v, :routing_key => @routing_key)
-				EventMachine.add_timer(2) do
-					connection.close { EventMachine.stop }
+			  	exchange.publish(v, :routing_key => @request_routing_key)
+				if (@response_routing_key)
+				  	@answer_queue = channel.queue(@response_routing_key, :auto_delete => true)
+				  	@answer_queue.bind(exchange, :routing_key => @response_routing_key)
+					@answer_queue.subscribe do |message|
+						puts "Received: #{message}"
+						connection.close { EventMachine.stop }
+					end
+				else
+					EventMachine.add_timer(2) do
+						connection.close { EventMachine.stop }
+					end
 				end
 			rescue => e
 				puts e
@@ -31,5 +41,5 @@ class LambdaQueuer
 	end
 end
 
-#LambdaQueuer.new('lambda_exchange', 'lambda').post {|all| all}
+LambdaQueuer.new(:exchange => 'lambda_exchange', :request_routing_key => 'lambda', :response_routing_key => 'lambda_response').post {|data| true}
 
