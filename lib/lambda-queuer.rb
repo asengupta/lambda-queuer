8299f6be6dd3a39fa7003258a9bd3a83d1b31103
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-09-19 15:10:13 +0530

    Exposing queue to allow normal AMQP operations like purge, etc.

--- lib/lambda-queuer.rb
+++ lib/lambda-queuer.rb
@@ -3,6 +3,7 @@ require 'amqp'
 require 'sourcify'
 
 class LambdaQueuer
+	attr_accessor :queue
 	def initialize(exchange, routing_key, host='127.0.0.1', port=5672)
 		@host = host
 		@port = port
@@ -16,8 +17,8 @@ class LambdaQueuer
 				connection = AMQP.connect(:host => @host, :port => @port)
 			  	channel = AMQP::Channel.new(connection)
 			  	exchange = channel.direct(@exchange, :auto_delete => true)
-			  	queue = channel.queue(@routing_key, :auto_delete => false, :passive => true)
-			  	queue.bind(exchange, :routing_key => @routing_key)
+			  	@queue = channel.queue(@routing_key, :auto_delete => false, :passive => true)
+			  	@queue.bind(exchange, :routing_key => @routing_key)
 				v = block.to_source
 			  	exchange.publish(v, :routing_key => @routing_key)
 				EventMachine.add_timer(2) do
